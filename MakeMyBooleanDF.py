b4940fb4f81ec323d0778fa0baea109560509ec7
Joy <129235760+JoysGitHubPortfolio@users.noreply.github.com>
2023-06-08 19:13:35 +0100

    Delete MakeMyBooleanDF.py

--- MakeMyBooleanDF.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def MakeMyBooleanDF(DataFrame, boolValues):
-    columns = DataFrame.columns
-    for col, val in zip(columns, boolValues):
-        DataFrame.loc[DataFrame[col] >= val, col] = int(1)
-        DataFrame.loc[DataFrame[col] != int(1), col] = 0
-    return DataFrame
